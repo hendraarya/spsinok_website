6c495ac27063c3e16ac8bee81a92f101631daf37
hendraarya <hendraarya23@gmail.com>
2022-07-31 16:51:10 +0700

    first commit

---
+++ src/components/navigationBar.js
@@ -0,0 +1,10 @@
+
+const NavigationBar = () => {
+    return (
+        <h1 className="text-3xl font-bold underline bg-blue-700">
+      Hello world! Hendra
+    </h1>
+    )
+}
+
+export default  NavigationBar;
