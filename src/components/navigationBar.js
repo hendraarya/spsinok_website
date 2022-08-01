b931eb1bdba39d696a8ef92cdf1d0aac38227b11
Hendra Arya <hendraarya23@gmail.com>
2022-08-02 06:30:08 +0700

    update logospsi

--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -1,10 +1,16 @@
-
+import Spsilogo from "../assets/images/logospsi.png";
 const NavigationBar = () => {
-    return (
-        <h1 className="text-3xl font-bold underline bg-blue-700">
-      Hello world! Hendra
-    </h1>
-    )
-}
-
-export default  NavigationBar;
+  return (
+    <nav className="bg-gradient-to-r from-white to-gray-900 px-6">
+      <div className="container">
+        <div className="flex flex-wrap">
+          <img src={Spsilogo} alt="SPSI" className="w-16 md:w-96" />
+          <h3 className="font-bold text-2xl from-neutral-800 py-6">
+            PUK <span className="text-">SPSI NOK</span>
+          </h3>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default NavigationBar;
